601a93a3de5f6deb1dcf309cdc3f48e8bb697154
Sahil <sahilff952@gmail.com>
2026-02-19 11:47:46 +0530

    Add files via upload

---
+++ nullSafety.dart
@@ -0,0 +1,11 @@
+
+  void main()
+{
+
+ //nullable type oiperator "?";
+  //also known as null safety
+  String name;//non nullable
+  String? name1;//can have null values
+  print(name1.toString());    
+  print(name1.hashCode);
+}
