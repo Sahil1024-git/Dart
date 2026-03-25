8357f97360d5f78b97e843dd412b81783c9a44ef
Sahil <sahilyd.work@gmail.com>
2026-03-25 10:00:08 +0530

    Add files via upload

---
+++ andDev/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/Screens/Home/home_screen.dart';
+
+void main() {
+runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomeScreen());
+  }
+}
